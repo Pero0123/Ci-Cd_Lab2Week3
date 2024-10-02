1dd7673e3b4179ca5f77d2c03521b9195658e068
G00422221@atu.ie <g00422221@atu.ie>
2024-10-02 15:18:15 +0100

    created ProductController

---
+++ src/main/java/ie/atu/lab2week3/ProductController.java
@@ -0,0 +1,24 @@
+package ie.atu.lab2week3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private List<Product> productList = new ArrayList<>();
+    public ProductController(){
+    //initialize sample product
+        productList.add(new Product("TV", "Electronics", 700, 700));
+        productList.add(new Product("Microwave", "Electronics",200,798));
+
+}
+    @GetMapping
+    public List<Product> getAllProducts(){
+        return productList;
+    }
+
+}
+
