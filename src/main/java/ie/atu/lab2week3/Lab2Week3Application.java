e98f19b63ef2734c41b7fcaeb744437572d43742
peros <g00422221@atu.ie>
2024-10-02 09:39:41 +0100

    created project

---
+++ src/main/java/ie/atu/lab2week3/Lab2Week3Application.java
@@ -0,0 +1,13 @@
+package ie.atu.lab2week3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab2Week3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab2Week3Application.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/lab2week3/Lab2Week3ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.lab2week3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab2Week3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
